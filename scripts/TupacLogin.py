add65f5e8bfedefa79dc3af9f3250f8a3081b171
Jose Augusto Zevallos Ruiz <JoseZevallosR@users.noreply.github.com>
2022-06-09 22:22:13 -0500

    adding

--- scripts/TupacLogin.py
+++ scripts/TupacLogin.py
@@ -7,6 +7,12 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+import os
+
+#mesh functions
+import sys
+#adding path to source directory
+sys.path.insert(0, 'd:/Proyectos_GitHub/dev_mode/src')
 
 # load and save dialog
 class LoadDialog(Widget):
@@ -37,9 +43,52 @@ class MeshWindow(Screen):
 		self._popup.open()
 
 	def load(self, path, filename):
-		with open(os.path.join(path, filename[0])) as stream:
-			self.text_input.text = stream.read()
+		self.ids.limit_layer.text=os.path.join(path, filename[0])
+		print(self.ids)		
 		self.dismiss_popup()
+
+	def mesh(self):
+		print('eres puto')
+		from geoVoronoi import createVoronoi
+		#Create mesh object
+		vorMesh = createVoronoi()
+
+		#Define base refinement and refinement levels
+		vorMesh.defineParameters(maxRef = 500, minRef=50, stages=5)
+
+		#Open limit layers and refinement definition layers
+		#vorMesh.addLimit('basin','../examples/In/shp/Angascancha_Basin_Extension.shp')
+		vorMesh.addLimit('basin',self.ids.limit_layer.text)
+		vorMesh.addLayer('facilities','../examples/In/shp/rios.shp')
+
+		#Generate point pair array
+		vorMesh.extractOrgVertices()
+
+		#Generate the point cloud and voronoi
+		vorMesh.createPointCloud()
+		vorMesh.generateVoronoi()
+
+		#check or create an output folder
+		outPath = '../examples/out/angascancha'
+
+		if os.path.isdir(outPath):
+		    print('The output folder %s exists'%outPath)
+		else:
+		    os.mkdir(outPath)
+		    print('The output folder %s has been generated.'%outPath)
+
+		#Export point data and voronoi polygons
+		#Points
+		vorMesh.getPointsAsShp('vertexOrg',outPath+'/vertexOrg.shp')
+		vorMesh.getPointsAsShp('vertexDist',outPath+'/vertexDist.shp')
+		vorMesh.getPointsAsShp('vertexBuffer',outPath+'/vertexBuffer.shp')
+		vorMesh.getPointsAsShp('vertexMaxRef',outPath+'/vertexMaxRef.shp')
+		vorMesh.getPointsAsShp('vertexMinRef',outPath+'/vertexMinRef.shp')
+		vorMesh.getPointsAsShp('vertexTotal',outPath+'/vertexTotal.shp')
+		#Polygons
+
+		vorMesh.getPolyAsShp('voronoiRegions',outPath+'/voronoiRegions.shp')
+		pass
 
 class MenuManager(ScreenManager):
 	pass
